b6cc917e444e61b822b83afd5a25b5170f6ab9e4
nathan-j-brenner <nathan.j.brenner@gmail.com>
2018-07-22 14:16:57 -0700

    switched for es6 const and arrow funcs

--- app.js
+++ app.js
@@ -1,54 +1,57 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var { graphiqlExpress } = require('apollo-server-express');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const { graphiqlExpress } = require("apollo-server-express");
 
-var indexRouter = require('./routes/index');
-var connect = require('./db');
-var { restRouter, graphQLRouter } = require('./api/index');
+const indexRouter = require("./routes/index");
+const connect = require("./db");
+const { restRouter, graphQLRouter } = require("./api/index");
+const config = require("./config/index");
+const db = mongoose.connection;
 
-var db = mongoose.connection;
-
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/api', restRouter);
-app.use('/graphql', express.json(), graphQLRouter);
-app.use('/docs', graphiqlExpress({endpointURL: '/graphql'}));
+app.use("/", indexRouter);
+app.post("/login", (req, res) => {
+	res.json({ login: true });
+});
+app.use("/api", restRouter);
+app.use("/graphql", express.json(), graphQLRouter);
+app.use("/docs", graphiqlExpress({ endpointURL: "/graphql" }));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use((err, req, res, next) => {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('mongodb connected')
-})
-connect()
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+	console.log(`mongodb connected, server started on port ${config.port}`);
+});
+connect();
 
 module.exports = app;
---
+++ readme.md
@@ -0,0 +1,5 @@
+# REST and GraphQl service for GiftGiver API
+
+## To start
+- `mongod`
+- `npm start`
